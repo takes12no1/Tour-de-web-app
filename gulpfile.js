e58bb3e14e358da4222c4dea0737bc36971f860f
takes12no1 <takes12no1@gmail.com>
2017-01-26 20:57:01 +0000

    added jquery

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
-    coffee = require('gulp-coffee');
+    coffee = require('gulp-coffee'),
+    browserify = require('gulp-browserify'),
+    concat = require('gulp-concat');
+    
     
 var coffeeSources = ['components/coffee/*.coffee'];
+var jsSources = ['components/scripts/*.js'];
 
 gulp.task('log', function(){
     gutil.log('Gulp Taken')
@@ -13,4 +17,11 @@ gulp.task('coffee', function(){
         .pipe(coffee({ bare: true })
         .on('error', gutil.log))
         .pipe(gulp.dest('components/scripts'))
+})
+
+gulp.task('js', function(){
+    gulp.src(jsSources)
+        .pipe(concat('script.js'))
+        .pipe(browserify())
+        .pipe(gulp.dest('builds/development/js'))
 })
