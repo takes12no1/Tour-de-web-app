1195cdf897a6fb8c9b632b020ccba09caa530511
takes12no1 <takes12no1@gmail.com>
2017-01-26 21:23:04 +0000

    Added sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,13 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
     concat = require('gulp-concat');
     
     
 var coffeeSources = ['components/coffee/*.coffee'];
 var jsSources = ['components/scripts/*.js'];
+var sassSources = ['components/sass/style.scss'];
 
 gulp.task('log', function(){
     gutil.log('Gulp Taken')
@@ -24,4 +26,15 @@ gulp.task('js', function(){
         .pipe(concat('script.js'))
         .pipe(browserify())
         .pipe(gulp.dest('builds/development/js'))
+})
+
+gulp.task('compass', function(){
+    gulp.src(sassSources)
+        .pipe(compass({
+            sass: 'components/sass',
+            image: 'builds/development/images',
+            style: 'compact'
+        })
+        .on('error', gutil.log))
+        .pipe(gulp.dest('builds/development/css'))
 })
