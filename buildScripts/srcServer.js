9d61f895fddf4b437af20a15affcd943f9a57cdd
Mark Coleman <mcoleman@primelending.com>
2017-10-11 21:02:52 -0500

    Added Bundling

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var path = require('path');
-var opn = require('opn');
+import express from 'express';
+import path from 'path';
+import opn from 'opn'
+import webpack from 'webpack';
+import config from '../webpack.config.dev';
 
-var port = 3000;
-var app = express();
+const port = 3000;
+const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+	noInfo: true,
+	publicPath: config.output.publicPath
+  }));
 
 app.get('/', function(req, res) {
 	res.sendFile(path.join(__dirname, '../src/index.html'));
